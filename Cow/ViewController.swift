f686514a39349958571c603930abf675a58a2543
admin <296019487@qq.com>
2021-08-04 13:43:56 +0800

    初始化项目

---
+++ Cow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cow
+//
+//  Created by admin on 2021/8/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
